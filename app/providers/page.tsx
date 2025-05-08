b9627a849bcb1e0193b6ab51dadee76ef06cbb00
neapolia <gurinapolina2003@gmail.com>
2025-05-09 00:58:59 +0300

    fix: add nodejs runtime to auth configuration

--- app/providers/page.tsx
+++ app/providers/page.tsx
@@ -1,32 +1,26 @@
+// app/providers/page.tsx
 import { Metadata } from "next";
 import { fetchFilteredProviders } from "@/app/lib/data";
 import ProvidersTable from "@/app/ui/customers/table";
 
-// Настройка метаданных страницы
+// Метаданные страницы
 export const metadata: Metadata = {
   title: "Поставщики",
 };
 
+// Компонент страницы
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query ?? "";
 
-export async function getServerSideProps(context: { query: { query?: string } }) {
-  const query = context.query.query || ""; 
-
-  // Получаем данные поставщиков
+  // Серверная загрузка поставщиков
   const providers = await fetchFilteredProviders(query);
 
-  // Возвращаем данные как props
-  return {
-    props: {
-      providers, // Поставщики будут переданы в компонент
-    },
-  };
-}
-
-// Компонент страницы, который отображает таблицу
-export default function Page({ providers }: { providers: any[] }) {
   return (
     <div className="w-full">
-      {/* Отображаем таблицу с поставщиками */}
       <ProvidersTable providers={providers} />
     </div>
   );
