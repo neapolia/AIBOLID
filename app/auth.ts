01bfa78b15b96cd21d4d2367d9c36b878bcee9e2
neapolia <gurinapolina2003@gmail.com>
2025-05-09 18:39:34 +0300

    fix: auth config for nodejs runtime

--- app/auth.ts
+++ app/auth.ts
@@ -79,6 +79,5 @@ export const authOptions: AuthOptions = {
 };
 
 export const { auth, signIn, signOut } = NextAuth(authOptions);
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+
 
