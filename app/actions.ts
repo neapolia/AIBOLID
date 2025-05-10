aaae9845c513eec713d35670305eb90064b72040
neapolia <gurinapolina2003@gmail.com>
2025-05-10 21:06:25 +0300

    fix: remove unused imports and fix getData function

--- app/actions.ts
+++ app/actions.ts
@@ -1,13 +1,10 @@
 // app/actions.ts
 "use server";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 import postgres from "postgres";
-import { z } from "zod";
 
 const sql = postgres(process.env.POSTGRES_URL!);
 
 export async function getData() {
-    const data = await sql`...`;
+    const data = await sql`SELECT * FROM polina_providers`;
     return data;
 }
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 import postgres from "postgres";
-import { z } from "zod";
 
 const sql = postgres(process.env.POSTGRES_URL!);
 
